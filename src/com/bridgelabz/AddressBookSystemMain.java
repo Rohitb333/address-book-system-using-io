b2f662f99778b842d1758e3bdeec9d2407e7c599
Rohit <rohitb333@gmail.com>
2022-04-12 21:45:05 +0530

    Added Contact File

--- src/com/bridgelabz/AddressBookSystemMain.java
+++ src/com/bridgelabz/AddressBookSystemMain.java
@@ -3,5 +3,10 @@ package com.bridgelabz;
 public class AddressBookSystemMain {
     public static void main(String[] args) {
         System.out.println("Welcome To AddressBook management System");
+
+        AddressBookSystemDetails details = new AddressBookSystemDetails();
+
+        details.addDetails();
+        details.display();
     }
 }
