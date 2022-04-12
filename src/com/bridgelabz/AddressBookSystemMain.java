c0d7c43a44d38147ddc6dacdc9956d2ae8fcd8a6
Rohit <rohitb333@gmail.com>
2022-04-12 21:37:31 +0530

    Welcome To AddressBook management System

---
+++ src/com/bridgelabz/AddressBookSystemMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class AddressBookSystemMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To AddressBook management System");
+    }
+}
